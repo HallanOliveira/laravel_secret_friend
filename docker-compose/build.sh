5a8445f7f84318d8badaad9489c85e7524421eb5
Hallan Silva <hallan_douglas@hotmail.com>
2024-05-23 20:35:31 -0300

    fix: ajustes para build do docker

--- docker-compose/build.sh
+++ docker-compose/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+cp ./.env.example ./.env
+cp ./docker-compose-example.yml ./docker-compose.yml
 docker compose up -d --build
+docker ps
+docker exec -i secret-friend-app chmod -R 777 storage storage/logs
 docker exec -i secret-friend-app composer install
 docker exec -i secret-friend-app php artisan key:generate
 docker exec -i secret-friend-app php artisan optimize
+docker exec -i secret-friend-app chmod -R 777 storage bootstrap/cache
+docker exec -i secret-friend-app php artisan migrate
 docker exec -i secret-friend-app npm install
 docker exec -i secret-friend-app npm run build
